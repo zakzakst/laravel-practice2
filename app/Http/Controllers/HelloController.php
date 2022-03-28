8df7e56aba06aeff1f97feca74142483b91f544f
zakzakst <e1433104@gmail.com>
2022-03-28 21:23:59 +0900

    リスト1-6,7

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index() {
+    public function index($id) {
         $data = [
-            'msg' => 'this is sample message.',
+            'msg' => 'id = ' . $id,
         ];
         return view('hello.index', $data);
     }
