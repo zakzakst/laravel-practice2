34242248fb1cdb6024c73f9ff143a33fed93c119
zakzakst <e1433104@gmail.com>
2022-04-06 04:20:22 +0900

    リスト1-54,55

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -16,11 +16,18 @@ class HelloController extends Controller
 
     public function index(Request $request) {
         $msg = 'please input text:';
+        $keys = [];
+        $values = [];
+
         if ($request->isMethod('post')) {
-            $msg = 'you typed: "' . $request->input('msg') . '"';
+            $form = $request->all();
+            $keys = array_keys($form);
+            $values = array_values($form);
         }
         $data = [
             'msg' => $msg,
+            'keys' => $keys,
+            'values' => $values,
         ];
         return view('hello.index', $data);
     }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,8 +8,22 @@
   <p>{!! $msg !!}</p>
   <form action="/hello" method="post">
     @csrf
-    <input type="text" name="msg">
+    <div>
+      NAME:<input type="text" name="name">
+    </div>
+    <div>
+      MAIL:<input type="text" name="mail">
+    </div>
+    <div>
+      TEL:<input type="text" name="tel">
+    </div>
     <input type="submit">
   </form>
+  <hr>
+  <ol>
+    @for($i = 0; $i < count($keys); $i++)
+      <li>{{ $keys[$i] }}:{{ $values[$i] }}</li>
+    @endfor
+  </ol>
 </body>
 </html>
