a071011eb8309964c44a27243cd39cd7862b8561
zakzakst <e1433104@gmail.com>
2022-04-06 04:26:15 +0900

    リスト1-56

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -14,15 +14,20 @@ class HelloController extends Controller
         $this->fname = 'hello.txt';
     }
 
-    public function index(Request $request) {
+    public function index(Request $request, Response $response) {
         $msg = 'please input text:';
         $keys = [];
         $values = [];
 
         if ($request->isMethod('post')) {
             $form = $request->all();
-            $keys = array_keys($form);
-            $values = array_values($form);
+            $result = '<html><body>';
+            foreach($form as $key => $value) {
+                $result .= $key . ': ' . $value . '<br>';
+            }
+            $result .= '</body></html>';
+            $response->setContent($result);
+            return $response;
         }
         $data = [
             'msg' => $msg,
