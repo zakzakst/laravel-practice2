85bb433518c5c928c94b6b7bde10ef9c1aa201a3
zakzakst <e1433104@gmail.com>
2022-04-07 03:12:30 +0900

    2-16

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        app()->bind('App\MyClasses\MyService', function($app) {
+        app()->singleton('App\MyClasses\MyService', function($app) {
             $myservice = new MyService();
             $myservice->setId(0);
             return $myservice;
