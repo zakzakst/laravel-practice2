6e6a4f28b4defc28ac001af01d10399f70659e3f
zakzakst <e1433104@gmail.com>
2022-04-07 04:28:17 +0900

    2-24

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        app()->bind('App\MyClasses\MyServiceInterface', 'App\MyClasses\PowerMyService');
+        app()->resolving(function($obj, $app) {
+            if (is_object($obj)) {
+                echo get_class($obj) . '<br>';
+            }
+            else {
+                echo $obj . '<br>';
+            }
+        });
+        app()->resolving(PowerMyService::class, function($obj, $app) {
+            $newdata = ['ハンバーグ', 'カレーライス', '唐揚げ', '餃子'];
+            $obj->setData($newdata);
+            $obj->setId(rand(0, count($newdata)));
+        });
+        app()->singleton('App\MyClasses\MyServiceInterface', 'App\MyClasses\PowerMyService');
     }
 }
