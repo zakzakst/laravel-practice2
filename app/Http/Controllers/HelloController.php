be510cfae5058c1f2dce9fa91dff48b318ec625a
zakzakst <e1433104@gmail.com>
2022-04-08 09:57:24 +0900

    3-4

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,10 +5,23 @@ use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-    public function index() {
-        $result = DB::table('people')->get();
+    public function index($id = -1) {
+        if ($id >= 0)
+        {
+            $msg = 'get ID <= ' . $id;
+            $result = DB::table('people')
+                ->where('id', '<=', $id)
+                ->get();
+        }
+        else
+        {
+            $msg = 'get people records.';
+            $result = DB::table('people')
+                ->get();
+        }
+        
         $data = [
-            'msg' => 'Database access.',
+            'msg' => $msg,
             'data' => $result,
         ];
         return view('hello.index', $data);
