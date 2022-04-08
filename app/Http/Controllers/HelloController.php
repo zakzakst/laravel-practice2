cf3d4b70fc1aeac74872d487b151d36226bc59dd
zakzakst <e1433104@gmail.com>
2022-04-08 10:00:03 +0900

    3-5

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,9 +8,9 @@ class HelloController extends Controller
     public function index($id = -1) {
         if ($id >= 0)
         {
-            $msg = 'get ID <= ' . $id;
+            $msg = 'get name like"' . $id . '".';
             $result = DB::table('people')
-                ->where('id', '<=', $id)
+                ->where('name', 'like', '%' . $id . '%')
                 ->get();
         }
         else
