7cdc68bf0935b3741a13930bd0303ec8eedbd7f1
zakzakst <e1433104@gmail.com>
2022-04-08 10:16:19 +0900

    3-7

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,10 +6,16 @@ use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
     public function index($id = -1) {
-        $msg = 'get people records.';
-        $first = DB::table('people')->first();
-        $last = DB::table('people')->orderBy('id', 'desc')->first();
-        $result = [$first, $last];
+        if ($id >= 0)
+        {
+            $msg = 'get name like "' . $id . '".';
+            $result = [DB::table('people')->find($id)];
+        }
+        else
+        {
+            $msg = 'get people records.';
+            $result = DB::table('people')->get();
+        }
         
         $data = [
             'msg' => $msg,
