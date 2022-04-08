ad52736fbcac3a60a864d08193d601822831c467
zakzakst <e1433104@gmail.com>
2022-04-09 08:37:04 +0900

    3-12

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,23 +6,11 @@ use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
     public function index($id = -1) {
-        $data = ['msg' => '', 'data' => []];
-        $msg = 'get: ';
-        $result = [];
-        $count = 0;
-        DB::table('people')
-            ->chunkById(3, function($items) use (&$msg, &$result, &$id, &$count)
-        {
-            if ($count == $id) {
-                foreach($items as $item) {
-                    $msg .= $item->id . ' ';
-                    $result += array_merge($result, [$item]);
-                }
-                return false;
-            }
-            $count++;
-            return true;
-        });
+        $ids = explode(',', $id);
+        $msg = 'get people.';
+        $result = DB::table('people')
+            ->whereBetween('id', $ids)
+            ->get();
         
         $data = [
             'msg' => $msg,
