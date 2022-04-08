c7c667583363a4f1ec683be210ad230849d36409
zakzakst <e1433104@gmail.com>
2022-04-08 09:32:08 +0900

    2-35

--- app/Http/Middleware/MyMiddleware.php
+++ app/Http/Middleware/MyMiddleware.php
@@ -17,6 +17,7 @@ class MyMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        // before処理開始
         $id = rand(0, count(MyService::alldata()));
         MyService::setId($id);
         $merge_data = [
@@ -25,7 +26,18 @@ class MyMiddleware
             'alldata' => MyService::alldata(),
         ];
         $request->merge($merge_data);
-        
-        return $next($request);
+
+        $response = $next($request);
+
+        // after処理開始
+        $content = $response->content();
+        $content .= '<style>
+            body { background-color: #eef; }
+            p { font-size: 18px; }
+            li { color: red; font-weight: bold; }
+        </style>';
+        $response->setContent($content);
+
+        return $response;
     }
 }
