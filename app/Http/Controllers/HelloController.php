6a05654b172373eb74eb175a70737950e395d3fa
zakzakst <e1433104@gmail.com>
2022-04-09 15:22:47 +0900

    3-29

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,12 +8,17 @@ class HelloController extends Controller
 {
     public function index(Request $request) {
         $msg = 'show people record.';
-        $result = Person::get()->reject(function($person) {
+        $result = Person::get()->filter(function($person) {
+            return $person->age < 50;
+        });
+        $result2 = Person::get()->filter(function($person) {
             return $person->age < 20;
         });
+        $result3 = $result->diff($result2);
+
         $data = [
             'msg' => $msg,
-            'data' => $result,
+            'data' => $result3,
         ];
         return view('hello.index', $data);
     }
