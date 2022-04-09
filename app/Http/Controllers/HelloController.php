fe8fd396379e267672a03570dbca0f4bacce8b6b
zakzakst <e1433104@gmail.com>
2022-04-09 09:09:10 +0900

    3-19

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,8 +8,7 @@ class HelloController extends Controller
     public function index(Request $request) {
         $id = $request->query('page');
         $msg = 'show page: ' . $id;
-        $result = DB::table('people')
-            ->paginate(3, ['*'], 'page', $id);
+        $result = DB::table('people')->simplePaginate(3);
         
         $data = [
             'msg' => $msg,
