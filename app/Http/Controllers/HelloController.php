c6621ee9918b5fbe029d71d87b9526785cfae67c
zakzakst <e1433104@gmail.com>
2022-04-09 15:30:10 +0900

    3-31

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,11 +8,13 @@ class HelloController extends Controller
 {
     public function index(Request $request) {
         $msg = 'show people record.';
-        $keys = Person::get()->modelKeys();
-        $even = array_filter($keys, function($key) {
-            return $key % 2 == 0;
+        $even = Person::get()->filter(function($item) {
+            return $item->id % 2 == 0;
         });
-        $result = Person::get()->only($even);
+        $even2 = Person::get()->filter(function($item) {
+            return $item->age % 2 == 0;
+        });
+        $result = $even->merge($even2);
 
         $data = [
             'msg' => $msg,
