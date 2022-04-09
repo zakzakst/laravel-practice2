25e874262419e1dfba948f7cd5c312ad240702fe
zakzakst <e1433104@gmail.com>
2022-04-09 09:06:50 +0900

    3-16,17,18

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-    public function index($id = -1) {
+    public function index(Request $request) {
+        $id = $request->query('page');
         $msg = 'show page: ' . $id;
         $result = DB::table('people')
             ->paginate(3, ['*'], 'page', $id);
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -2,6 +2,7 @@
 <html lang="ja">
 <head>
   <title>Index</title>
+  <link href="/css/app.css" rel="stylesheet">
 </head>
 <body>
   <h1>Hello/Index</h1>
@@ -11,5 +12,6 @@
       <li>{{ $item->name }} [{{ $item->mail }},{{ $item->age }}]</li>
     @endforeach
   </ol>
+  {!! $data->links() !!}
 </body>
 </html>
