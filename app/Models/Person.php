4276211938750eda1ddd9d2eb07182817e50ed46
zakzakst <e1433104@gmail.com>
2022-04-11 22:50:00 +0900

    3-33,34

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     use HasFactory;
+
+    public function newCollection(array $models = []) {
+        return new MyCollection($models);
+    }
+}
+
+class MyCollection extends Collection
+{
+    public function fields() {
+        $item = $this->first();
+        return array_keys($item->toArray());
+    }
 }
