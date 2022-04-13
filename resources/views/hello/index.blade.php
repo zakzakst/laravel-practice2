ff8218e0a3a07829b2a15f3adb3815bd3105ccaa
zakzakst <e1433104@gmail.com>
2022-04-13 18:59:39 +0900

    3-35,36

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,9 +10,7 @@
   @foreach($data as $item)
     <tr>
       <th>{{ $item->id }}</th>
-      <td>{{ $item->name }}</td>
-      <td>{{ $item->mail }}</td>
-      <td>{{ $item->age }}</td>
+      <td>{{ $item->name_and_age }}</td>
     </tr>
   @endforeach
 </body>
