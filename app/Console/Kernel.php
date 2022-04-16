960dda6f53aaf20d6a3cf7614c6feb09e727e1fb
zakzakst <e1433104@gmail.com>
2022-04-16 16:34:42 +0900

    4-29

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,27 +2,23 @@
 
 namespace App\Console;
 
+use App\Person;
+use App\Jobs\MyJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('queue:work --stop-when-empty');
+        $count = Person::all()->count();
+        $id = rand(0, $count) + 1;
+        $schedule->call(function() use ($id) {
+            $person = Person::find($id);
+            MyJob::dispatch($person);
+        });
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
