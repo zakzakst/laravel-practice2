76e7887891d9877a8e63fd7beef6e8e9e50a1e63
zakzakst <e1433104@gmail.com>
2022-04-16 16:30:45 +0900

    4-28

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->exec('mycmd');
+        $schedule->command('queue:work --stop-when-empty');
     }
 
     /**
