39ed6e2a266b7ae2cdc7dd5e593d418e721cea47
zakzakst <e1433104@gmail.com>
2022-04-17 20:54:03 +0900

    6-11,12

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,21 +7,27 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    use RefreshDatabase;
+    
     public function test_example()
     {
         $data = [
             'id' => 1,
-            'name' => 'YAMADA-TARO',
-            'mail' => 'taro@yamada',
-            'age' => '34',
+            'name' => 'DUMMY',
+            'mail' => 'dummy@mail',
+            'age' => 0,
         ];
+        $person = new Person();
+        $person->fill($data)->save();
         $this->assertDatabaseHas('people', $data);
-        $data['id'] = 2;
+
+        $person->name = 'NOT-DUMMY';
+        $person->save();
+        $this->assertDatabaseMissing('people', $data);
+        $data['name'] = 'NOT-DUMMY';
+        $this->assertDatabaseHas('people', $data);
+
+        $person->delete();
         $this->assertDatabaseMissing('people', $data);
     }
 }
