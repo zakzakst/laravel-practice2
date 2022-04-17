b9f515919dba6706613824cd5a54fecdb85cdc2f
zakzakst <e1433104@gmail.com>
2022-04-17 21:39:40 +0900

    6-17,18

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -9,3 +9,15 @@ $factory->define(Model::class, function(Faker $faker) {
     'age' => $faker->numberBetween(1, 100),
   ];
 });
+
+$factory->state(Person::class, 'upper', function($faker) {
+  return [
+    'name' => strtoupper($faker->name()),
+  ];
+});
+
+$factory->state(Person::class, 'lower', function($faker) {
+  return [
+    'name' => strtolower($faker->name()),
+  ];
+});
