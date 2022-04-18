eea527f15a59f07500207fad4e84b3bf58fce983
zakzakst <e1433104@gmail.com>
2022-04-18 22:24:01 +0900

    6-20

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,9 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Person;
+use Illuminate\Support\Facades\Bus;
+use App\Jobs\MyJob;
 
 class ExampleTest extends TestCase
 {
@@ -11,31 +14,20 @@ class ExampleTest extends TestCase
 
     public function test_example()
     {
-        $list = [];
-        for($i = 0; $i < 10; $i++) {
-            $p1 = factory(Person::class)->create();
-            $p2 = factory(Person::class)->states('upper')->create();
-            $p3 = factory(Person::class)->states('lower')->create();
-            $p4 = factory(Person::class)->states('upper')->states('lower')->create();
-            $list = array_merge($list, [
-                $p1->id,
-                $p2->id,
-                $p3->id,
-                $p4->id,
-            ]);
-        }
+        $id = 1;
+        $data = [
+            'id' => $id,
+            'name' => 'DUMMY',
+            'mail' => 'dummy@mail',
+            'age' => 0,
+        ];
+        $person = new Person();
+        $person->fill($data)->save();
+        $this->assertDatabaseHas('people', $data);
 
-        for($i = 0; $i < 10; $i++) {
-            shuffle($list);
-            $item = array_shift($list);
-            $person = Person::find($item);
-            $data = $person->toArray();
-            print_r($data);
-
-            $this->assertDatabaseHas('people', $data);
-
-            $person->delete();
-            $this->assertDatabaseMissing('people', $data);
-        }
+        Bus::fake();
+        Bus::assertNotDispatched(MyJob::class);
+        MyJob::dispatch($id);
+        Bus::assertDispatched(MyJob::class);
     }
 }
